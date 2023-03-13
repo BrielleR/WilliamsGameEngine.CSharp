6a7c371be32cdddec452731eb5d25515c8321d64
Brielle Roze <rozebri@rsd407.org>
2023-03-13 08:34:52 -0700

    3/13/23

---
+++ MyGame/Ship.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+/// 
+namespace MyGame
+{
+	class Ship : GameObject
+	{
+		private readonly Sprite _sprite = new Sprite();
+
+		Ship()
+		{
+			_sprite.Texture = Game.GetTextrue("");
+			_sprite.Position = new Vector2f(100, 100);
+		}
+		public override Draw();
+		{
+		Game.RenderWindow.Draw(_sprite);
+		}
+	public override Update(Time elapsed)
+	{
+		Vector2f pos = _sprite.Position;
+		float x = pos.X;
+		float y = pos.Y;
+
+		int msElapsed = elapsed.AsMilisecond();
+
+
+	}
+
+	}
+}
