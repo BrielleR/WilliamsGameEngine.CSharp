6914549041329e6346b6c1707e757cf9ea08d233
Brielle Roze <rozebri@rsd407.org>
2023-03-13 10:38:04 -0700

    3/13/2023  10:37 am

--- MyGame/Ship.cs
+++ MyGame/Ship.cs
@@ -1,51 +1,59 @@
-﻿using System;
+﻿using GameEngine;
+using SFML.System;
+using SFML.Window;
+using SFML.Graphics;
+using System;
+using System.Runtime.CompilerServices;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-/// 
+/*
+ * Part one of Goal Sheet 13 : Class for space ship
+ * Maintence Log:
+ * Date:         Done:
+ * 3/13/23       Started working on class but have 56 errors, now 36 errors, now 5 - just the names of the funtions all ahve errors.
+ */
 namespace MyGame
 {
-	class Ship : GameObject
+	class Ship //GameObject
 	{
 		private readonly Sprite _sprite = new Sprite();
-
 		Ship()
 		{
-			_sprite.Texture = Game.GetTextrue("");
+			_sprite.Texture = Game.GetTexture("Resources/ship.png");
 			_sprite.Position = new Vector2f(100, 100);
 		}
-		public override Draw();
+	
+		public override void Draw()
 		{
 		Game.RenderWindow.Draw(_sprite);
 		}
-	    public override Update(Time elapsed)
+		public override void Update(Time elapsed, int speed)
 	    {
 	    	Vector2f pos = _sprite.Position;
 	    	float x = pos.X;
 	    	float y = pos.Y;
 	    
-	    	int msElapsed = elapsed.AsMilisecond();
+	    	int msElapsed = elapsed.AsMilliseconds();			
 	    
-	    	if(Keyboard.IsKeyPressed(Keyboard.Key.Up)) { y -= speed * msElapsed; }
-	    	if(Keyboard.IsKeyPressed(Keyboard.Key.Down)) { y += spped * msElapsed; }
-	    	if(Keyboard.IsKeyPressed(Keyboard.Key.Left)) { x -= speed * msElapsed; }
-	    	if(Keyboard.IsKeyPressed(Keyboard.Key.Right)) { x += speed * msElapsed; }
+	    	if (Keyboard.IsKeyPressed(Keyboard.Key.Up)) { y -= speed * msElapsed; }
+	    	if (Keyboard.IsKeyPressed(Keyboard.Key.Down)) { y += speed * msElapsed; }
+	    	if (Keyboard.IsKeyPressed(Keyboard.Key.Left)) { x -= speed * msElapsed; }
+	    	if (Keyboard.IsKeyPressed(Keyboard.Key.Right)) { x += speed * msElapsed; }
 			_sprite.Position = new Vector2f(x, y);
 	    }
-	}
-}
-        public override void Update(Time elapsed)
+        public override void Update(Time elapsed, int speed)
         {
             Vector2f pos = _sprite.Position;
             float x = pos.X;
             float y = pos.Y;
-        
-            int msElapsed = elapsed.AsMilisecond();
-        
+
+            int msElapsed = elapsed.AsMilliseconds();
+
             if (Keyboard.IsKeyPressed(Keyboard.Key.Up)) { y -= speed * msElapsed; }
-            if (Keyboard.IsKeyPressed(Keyboard.Key.Down)) { y += spped * msElapsed; }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Down)) { y += speed * msElapsed; }
             if (Keyboard.IsKeyPressed(Keyboard.Key.Left)) { x -= speed * msElapsed; }
             if (Keyboard.IsKeyPressed(Keyboard.Key.Right)) { x += speed * msElapsed; }
             _sprite.Position = new Vector2f(x, y);
         }
+    }
+}
+       
